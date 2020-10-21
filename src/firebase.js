04e5aab006c32a5b482c4a3742cfcd78740e7e82
awantech-io <hello.wanlab@gmail.com>
2020-10-22 07:50:04 +0800

    app component

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC0DigQ9A5fQtTzW9bDdc71c2ihUco2gGU",
+  authDomain: "herwan-tiktok.firebaseapp.com",
+  databaseURL: "https://herwan-tiktok.firebaseio.com",
+  projectId: "herwan-tiktok",
+  storageBucket: "herwan-tiktok.appspot.com",
+  messagingSenderId: "687247858864",
+  appId: "1:687247858864:web:7abd39ecf57a5e290b2f75",
+  measurementId: "G-NHYN6YMEP1"
+};
